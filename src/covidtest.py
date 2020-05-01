a4d314e502321e8ecf04106c95489c23a4c212c0
Saul Rubio <saulrubio98@hotmail.com>
2020-05-01 18:05:52 -0500

    [FIX] COVID API menu

--- src/covidtest.py
+++ src/covidtest.py
@@ -48,19 +48,6 @@ class CovidTest():
         while(stay):
             try:
                 country = input("Please capture the name of the country: ")
-            print("==== Welcome to the Corona Virus Detector ===")
-            print("Please choose a valid option: ")
-            print("1. Start test")
-            print("2. Information about the Coronavirus Cases in México")
-            print("3. Exit")
-            print("============================================")
-            opt = int(input())
-            if(opt == 1):
-                self.startEngine()
-            elif(opt == 2):
-                print("something")
-            elif(opt == 3):
-                print("finish")
                 stay = False
                 self.apicovid.getInfo(country)
             except:
