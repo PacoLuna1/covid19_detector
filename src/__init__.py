1804fec764c4cb88278a5b6c0c0d1e2a5f4f61c3
Saul Rubio <saulrubio98@hotmail.com>
2020-05-01 19:39:36 -0500

    [ADD] Colors and updated menu

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,11 @@
 # coding=utf-8
 #!/usr/bin/python
+import os
+import colorama
 
 ### Own files
 from src.detector  import Detector, Option
 from src.api_covid import APICOVID
+
+### Import all colors
+from src.colors import *
--- src/covidtest.py
+++ src/covidtest.py
@@ -1,40 +1,57 @@
 # coding=utf-8
 #!/usr/bin/python
-from  src.__init__ import Detector, Option, APICOVID
+from  src.__init__ import *
 
 class CovidTest():
 
     def __init__(self):
         self.name = "CovidTest"
         self.apicovid = APICOVID()
+        self.globalStats = self.apicovid.getGlobalStats()
+
+
+    def cleanScreen(self):
+        os.system('clear')
+
+    def enterToContinue(self):
+        input(line + "Enter to continue...")
+        print(line)
 
     def menu(self):
         stay = True
-        invalidValue = "--> Please enter a valid value"
+        invalidValue = red + "--> Please enter a valid value ✗" + RESET + line
+        validValue = green + "--> Valid value ✓" + RESET + line
         opt1 = "1. Start test"
         opt2 = "2. Information about the Coronavirus"
-        opt3 = "0. Exit"
-        line = "\n"
+        opt3 = "3. Worldwide cases"
+        opt0 = "0. Exit"
+        byebye= "See you soon! Stay at home, stay healthy! ❤️ "
 
         while(stay):
-            print("==== Welcome to the Corona Virus Detector ===")
-            print("Please choose a valid option: ")
-            print(opt1 + line + opt2 + line + opt3)
-            print("============================================")
+            print(yellow + "==== Welcome to the Corona Virus Detector ===" + RESET)
+            print(opt0 + line+ opt1 + line + opt2 + line + opt3)
+            print(yellow + "============================================" + RESET)
             try:
-                opt = int(input())
-
+                opt = int(input("Select an option: "))
                 if(opt == 1):
+                    print(validValue)
                     self.startEngine()
                 elif(opt == 2):
+                    print(validValue)
                     self.consultInfo(invalidValue)
+                elif(opt == 3):
+                    print(validValue)
+                    self.consultWorldWide()
                 elif(opt == 0):
-                    print("finish")
+                    print(validValue + byebye)
                     stay = False
+                    self.enterToContinue()
                 else:
                     print(invalidValue)
             except:
                 print(invalidValue)
+                self.enterToContinue()
+                self.cleanScreen()
 
     def startEngine(self):
         engine = Detector()
@@ -42,6 +59,7 @@ class CovidTest():
         print("Test to know if the user is infected with Covid-19")
         engine.declare(Option(symptom=input("Which is your first symptom? : ")))
         engine.run()
+        self.cleanScreen()
 
     def consultInfo(self, invalidValue):
         stay = True
@@ -50,5 +68,16 @@ class CovidTest():
                 country = input("Please capture the name of the country: ")
                 stay = False
                 self.apicovid.getInfo(country)
+                input("Enter to continue...")
             except:
                 print(invalidValue)
+        self.cleanScreen()
+
+    def consultWorldWide(self):
+        print(yellow + "============================================" + RESET)
+        print(cyan + "Worldwide cases: " + RESET + str(self.globalStats['TotalCases']))
+        print(cyan +"Worldwide deaths: " + RESET + str(self.globalStats['GlobalDeaths']))
+        print(cyan +"Worldwide recoverements: " + RESET + str(self.globalStats['GlobalRecovered']))
+        print(yellow + "============================================" + RESET)
+        self.enterToContinue()
+        self.cleanScreen()
